90573b6dbab1d0fe97322c3eea9e3507993498d4
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-02-26 23:41:56 +0000

    NetworkState utils/listeners
    git-svn-id: http://svn.vuze.com/public/android@39863 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/VuzeRemoteApp.java
+++ remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/VuzeRemoteApp.java
@@ -1,26 +1,38 @@
 package com.vuze.android.remote;
 
 import android.app.Application;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 
 public class VuzeRemoteApp
 	extends Application
 {
 	private static AppPreferences appPreferences;
+	private static NetworkState networkState;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
 		if (AndroidUtils.DEBUG) {
 			Log.d(null, "Application.onCreate");
 		}
 		appPreferences = AppPreferences.createAppPreferences(getApplicationContext());
+		networkState = new NetworkState(getApplicationContext());
 	}
 	
+	@Override
+	public void onTerminate() {
+		// NOTE: This is never called except in emulation!
+		networkState.dipose();
+
+		super.onTerminate();
+	}
+
 	public static AppPreferences getAppPreferences() {
 		return appPreferences;
+	}
+
+	public static NetworkState getNetworkState() {
+		return networkState;
 	}
 }
