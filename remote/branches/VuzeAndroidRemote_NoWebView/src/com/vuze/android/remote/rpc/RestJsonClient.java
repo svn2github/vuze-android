85e25938119be12ac5d2e1955e0fe71595c7e929
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-03-12 04:38:52 +0000

    Send posts gzip'd (if big enough and supported), accept gzip results
    git-svn-id: http://svn.vuze.com/public/android@39952 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/rpc/RestJsonClient.java
+++ remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/rpc/RestJsonClient.java
@@ -29,11 +29,15 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpProtocolParams;
 
+import android.annotation.TargetApi;
+import android.net.http.AndroidHttpClient;
+import android.os.Build;
 import android.util.Log;
 
 import com.aelitis.azureus.util.JSONUtils;
@@ -46,18 +50,22 @@ import com.vuze.android.remote.AndroidUtils;
 public class RestJsonClient
 {
 	private static final String TAG = "RPC";
-	
+
 	private static final boolean DEBUG_DETAILED = false;
+
+	private static final boolean USE_STRINGBUFFER = true;
 
 	public static Object connect(String url)
 			throws RPCException {
-		return connect("", url, null, null, null);
+		return connect("", url, null, null, null, false);
 	}
 
-	public static Map<?, ?> connect(String id, String url, Map<?, ?> jsonPost, Header[] headers,
-			UsernamePasswordCredentials creds)
+	public static Map<?, ?> connect(String id, String url, Map<?, ?> jsonPost,
+			Header[] headers, UsernamePasswordCredentials creds, boolean sendGzip)
 			throws RPCException {
-		if (AndroidUtils.DEBUG) {
+		long readTime = 0;
+		int bytesRead = 0;
+		if (DEBUG_DETAILED) {
 			Log.d(TAG, id + "] Execute " + url);
 		}
 		long now = System.currentTimeMillis();
@@ -83,11 +91,17 @@ public class RestJsonClient
 				if (AndroidUtils.DEBUG) {
 					Log.d(TAG, id + "]  Post: " + postString);
 				}
-				post.setEntity(new StringEntity(postString));
+
+				AbstractHttpEntity compressedEntity = (sendGzip && Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO)
+						? getCompressedEntity(postString) : new StringEntity(postString);
+				post.setEntity(compressedEntity);
+
 				post.setHeader("Accept", "application/json");
 				post.setHeader("Content-type",
 						"application/x-www-form-urlencoded; charset=UTF-8");
 			}
+
+			setupRequestFroyo(httpRequest);
 
 			if (headers != null) {
 				for (Header header : headers) {
@@ -102,10 +116,10 @@ public class RestJsonClient
 
 			long then = System.currentTimeMillis();
 			if (DEBUG_DETAILED) {
-  			if (AndroidUtils.DEBUG) {
-  				Log.d(TAG, id + "]  conn ->" + (then - now) + "ms");
-  			}
-  			now = then;
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, id + "]  conn ->" + (then - now) + "ms");
+				}
+				now = then;
 			}
 
 			HttpEntity entity = response.getEntity();
@@ -114,71 +128,100 @@ public class RestJsonClient
 
 			if (entity != null) {
 
+				long contentLength = entity.getContentLength();
+				if (contentLength >= Integer.MAX_VALUE - 2) {
+					throw new RPCException("JSON response too large");
+				}
+
 				// A Simple JSON Response Read
-				InputStream instream = entity.getContent();
+				InputStream instream = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO)
+						? getUngzippedContent(entity) : entity.getContent();
 				InputStreamReader isr = new InputStreamReader(instream, "utf8");
-				StringBuilder sb = new StringBuilder();
-				//BufferedReader br = new BufferedReader(isr);
-				//br.mark(32767);
+
+				StringBuilder sb;
+				BufferedReader br;
+				if (USE_STRINGBUFFER) {
+					// Setting capacity saves StringBuffer from going through many
+					// enlargeBuffers, and hopefully allows toString to not make a copy
+					sb = new StringBuilder(contentLength > 512 ? (int) contentLength + 2
+							: 512);
+				} else {
+					br = new BufferedReader(isr);
+					br.mark(32767);
+				}
 
 				try {
-					
-  				char c[] = new char[8192];
-  				while (true) {
-  						int read = isr.read(c);
-  						if (read < 0) {
-  							break;
-  						}
-  						sb.append(c, 0, read);
-  				}
-  				
 
-  				if (DEBUG_DETAILED) {
-  					then = System.currentTimeMillis();
-  					if (AndroidUtils.DEBUG) {
-  						Log.d(TAG, id + "] " + sb.toString());
-  						Log.d(TAG, id + "]  read ->" + (then - now) + "ms");
-  					}
-  					now = then;
-  				}
+					// 9775 files on Nexus 7
+					// fastjson 1.1.39 (String)       :  924-1054ms
+					// fastjson 1.1.39 (StringBuilder): 1227-1463ms
+					// fastjson 1.1.39 (BR)           : 2233-2260ms
+					// fastjson 1.1.39 (isr)          :      2312ms
+					// GSON 2.2.4 (String)            : 1539-1760ms
+					// GSON 2.2.4 (BufferedReader)    : 2646-3060ms
 
-  				
-					// 9775 files
-					// 33xx-3800 for simple; 22xx for GSON 2.2.4; 18xx-19xx for fastjson 1.1.34
-//					json = JSONUtils.decodeJSON(br);
-  				json = JSONUtils.decodeJSON(sb.toString());
+					if (USE_STRINGBUFFER) {
+						char c[] = new char[8192];
+						while (true) {
+							int read = isr.read(c);
+							if (read < 0) {
+								break;
+							}
+							sb.append(c, 0, read);
+						}
 
+						if (AndroidUtils.DEBUG) {
+							then = System.currentTimeMillis();
+							if (DEBUG_DETAILED) {
+								Log.d(TAG, id + "] " + sb.toString());
+							}
+							bytesRead = sb.length();
+							readTime = (then - now);
+							now = then;
+						}
+
+						json = JSONUtils.decodeJSON(sb.toString());
+						//json = JSONUtilsGSON.decodeJSON(sb.toString());
+					} else {
+
+						//json = JSONUtils.decodeJSON(isr);
+						json = JSONUtils.decodeJSON(br);
+						//json = JSONUtilsGSON.decodeJSON(br);
+					}
 
 				} catch (Exception pe) {
 
 					try {
+						String line;
+						if (USE_STRINGBUFFER) {
+							line = sb.subSequence(0, Math.min(128, sb.length())).toString();
+						} else {
+							br.reset();
+							line = br.readLine().trim();
+						}
 
-//  					br.reset();
-//  					String line = br.readLine().trim();
 						isr.close();
-						String line = sb.subSequence(0, Math.min(128, sb.length())).toString();
-  
-  					if (AndroidUtils.DEBUG) {
-  						Log.d(TAG, id + "]line: " + line);
-  					}
-  					Header contentType = entity.getContentType();
-  					if (line.startsWith("<")
-  							|| line.contains("<html")
-  							|| (contentType != null && contentType.getValue().startsWith(
-  									"text/html"))) {
-  						// TODO: use android strings.xml
-  						throw new RPCException(
-  								response,
-  								"Could not retrieve remote client location information.  The most common cause is being on a guest wifi that requires login before using the internet.");
-  					}
+
+						if (AndroidUtils.DEBUG) {
+							Log.d(TAG, id + "]line: " + line);
+						}
+						Header contentType = entity.getContentType();
+						if (line.startsWith("<")
+								|| line.contains("<html")
+								|| (contentType != null && contentType.getValue().startsWith(
+										"text/html"))) {
+							// TODO: use android strings.xml
+							throw new RPCException(
+									response,
+									"Could not retrieve remote client location information.  The most common cause is being on a guest wifi that requires login before using the internet.");
+						}
 					} catch (IOException io) {
-						
+
 					}
 
 					throw new RPCException(pe);
 				} finally {
-					isr.close();
-//					br.close();
+					closeOnNewThread(USE_STRINGBUFFER ? isr : br);
 				}
 
 				if (AndroidUtils.DEBUG) {
@@ -196,9 +239,46 @@ public class RestJsonClient
 
 		if (AndroidUtils.DEBUG) {
 			long then = System.currentTimeMillis();
-			Log.d(TAG, id + "]  " + (DEBUG_DETAILED ? "parse" : "received in")  +" ->" + (then - now) + "ms");
+			Log.d(TAG, id + "] read " + bytesRead + " in " + readTime
+					+ ", parsed in " + (then - now) + "ms");
 		}
 		return json;
+	}
+
+	@TargetApi(Build.VERSION_CODES.FROYO)
+	private static AbstractHttpEntity getCompressedEntity(String data)
+			throws UnsupportedEncodingException {
+		try {
+			return AndroidHttpClient.getCompressedEntity(data.getBytes("UTF-8"), null);
+		} catch (Throwable e) {
+			return new StringEntity(data);
+		}
+	}
+
+	// HttpEntity.getContent().close can take 30s!
+	private static void closeOnNewThread(final Reader reader) {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					reader.close();
+				} catch (Throwable t) {
+				}
+			}
+		}, "closeInputStream");
+		thread.setDaemon(true);
+		thread.start();
+	}
+
+	@TargetApi(Build.VERSION_CODES.FROYO)
+	private static InputStream getUngzippedContent(HttpEntity entity)
+			throws IOException {
+		return AndroidHttpClient.getUngzippedContent(entity);
+	}
+
+	@TargetApi(Build.VERSION_CODES.FROYO)
+	private static void setupRequestFroyo(HttpRequestBase httpRequest) {
+		AndroidHttpClient.modifyRequestToAcceptGzipResponse(httpRequest);
 	}
 
 }
