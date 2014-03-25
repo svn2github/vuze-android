99b2f68568a57baf49e6f9f1d2e7cadf26bd626e
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-03-25 18:47:40 +0000

    cache clearing options
    git-svn-id: http://svn.vuze.com/public/android@40007 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/SessionInfoManager.java
+++ remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/SessionInfoManager.java
@@ -8,9 +8,13 @@ import android.util.Log;
 
 public class SessionInfoManager
 {
+	private static final String TAG = "SessionInfoManager";
+
 	public static String BUNDLE_KEY = "RemoteProfileID";
 
 	private static Map<String, SessionInfo> mapSessionInfo = new HashMap<String, SessionInfo>();
+
+	private static String lastUsed;
 
 	public static SessionInfo getSessionInfo(String id, Activity activity,
 			boolean rememberSettingChanges) {
@@ -30,11 +34,13 @@ public class SessionInfoManager
 				Log.d("SessionInfo", "setting SessionInfo for " + id);
 
 			}
+			lastUsed = id;
 			return sessionInfo;
 		}
 	}
 	
 	public static void removeSessionInfo(String id) {
+		lastUsed = null;
 		synchronized (mapSessionInfo) {
 			mapSessionInfo.remove(id);
 		}
@@ -50,8 +56,38 @@ public class SessionInfoManager
 						rememberSettingChanges);
 				mapSessionInfo.put(id, sessionInfo);
 			}
+			lastUsed = id;
 			return sessionInfo;
 		}
 	}
 
+	public static void clearTorrentCaches(boolean keepLastUsed) {
+		int numClears = 0;
+		synchronized (mapSessionInfo) {
+			for (String key : mapSessionInfo.keySet()) {
+				if (keepLastUsed && key.equals(lastUsed)) {
+					continue;
+				}
+				SessionInfo sessionInfo = mapSessionInfo.get(key);
+				sessionInfo.clearTorrentCache();
+				numClears++;
+			}
+		}
+		if (AndroidUtils.DEBUG) {
+			Log.d(TAG, "clearTorrentCache. " + numClears + " removed");
+		}
+	}
+
+	public static void clearTorrentFilesCaches(boolean keepLastUsedTorrentFiles) {
+		int numClears = 0;
+		synchronized (mapSessionInfo) {
+			for (String key : mapSessionInfo.keySet()) {
+				SessionInfo sessionInfo = mapSessionInfo.get(key);
+				numClears += sessionInfo.clearTorrentFilesCaches(keepLastUsedTorrentFiles);
+			}
+		}
+		if (AndroidUtils.DEBUG) {
+			Log.d(TAG, "clearTorrentFilesCaches. " + numClears + " removed");
+		}
+	}
 }
