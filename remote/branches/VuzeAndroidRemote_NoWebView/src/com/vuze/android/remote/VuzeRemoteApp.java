4b4153a8008a98128fbf52600e8965917e605e8c
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-03-25 18:51:48 +0000

    Memory Management
    git-svn-id: http://svn.vuze.com/public/android@40009 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/VuzeRemoteApp.java
+++ remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/VuzeRemoteApp.java
@@ -1,13 +1,17 @@
 package com.vuze.android.remote;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
 public class VuzeRemoteApp
 	extends Application
 {
+	private static final String TAG = "App";
+
 	private static AppPreferences appPreferences;
 
 	private static NetworkState networkState;
@@ -19,7 +23,7 @@ public class VuzeRemoteApp
 		super.onCreate();
 
 		if (AndroidUtils.DEBUG) {
-			Log.d(null, "Application.onCreate");
+			Log.d(TAG, "Application.onCreate");
 		}
 		applicationContext = getApplicationContext();
 		appPreferences = AppPreferences.createAppPreferences(applicationContext);
@@ -34,8 +38,68 @@ public class VuzeRemoteApp
 		}
 
 		appPreferences.setNumOpens(appPreferences.getNumOpens() + 1);
+		
 	}
 
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	@Override
+	public void onTrimMemory(int level) {
+		super.onTrimMemory(level);
+		switch (level) {
+			case TRIM_MEMORY_UI_HIDDEN: // not really a low memory event
+				break;
+			case TRIM_MEMORY_BACKGROUND: // app moved to background
+				break;
+			case TRIM_MEMORY_MODERATE:
+				// app in middle of background list 
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, "onTrimMemory Moderate");
+				}
+				SessionInfoManager.clearTorrentFilesCaches(true);
+				break;
+			case TRIM_MEMORY_COMPLETE: 
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, "onTrimMemory Complete");
+				}
+				// app next to be killed unless more mem found
+				SessionInfoManager.clearTorrentCaches(false); // clear all
+				break;
+			case TRIM_MEMORY_RUNNING_MODERATE:
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, "onTrimMemory RunningModerate");
+				}
+				SessionInfoManager.clearTorrentCaches(true); // clear all except current
+				break;
+			case TRIM_MEMORY_RUNNING_LOW : // Low memory
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, "onTrimMemory RunningLow");
+				}
+				SessionInfoManager.clearTorrentCaches(true); // clear all except current
+				SessionInfoManager.clearTorrentFilesCaches(true); // clear all except last file
+				break;
+			case TRIM_MEMORY_RUNNING_CRITICAL:
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, "onTrimMemory RunningCritical");
+				}
+				SessionInfoManager.clearTorrentCaches(true); // clear all except current
+				SessionInfoManager.clearTorrentFilesCaches(true); // clear all except last file
+				break;
+			default:
+				if (AndroidUtils.DEBUG) {
+					Log.d(TAG, "onTrimMemory " + level);
+				}
+		}
+	}
+	
+	@Override
+	public void onLowMemory() {
+		if (AndroidUtils.DEBUG) {
+			Log.d(TAG, "onLowMemory");
+		}
+		super.onLowMemory();
+		SessionInfoManager.clearTorrentCaches(false);
+	}
+	
 	public int pxToDp(int px) {
 		DisplayMetrics dm = getContext().getResources().getDisplayMetrics();
 
