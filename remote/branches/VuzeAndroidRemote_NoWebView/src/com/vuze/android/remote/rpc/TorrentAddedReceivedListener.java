256d13a291932620cc605714b33f985348447d39
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-03-31 23:18:57 +0000

    remove warning
    git-svn-id: http://svn.vuze.com/public/android@40054 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/rpc/TorrentAddedReceivedListener.java
+++ remote/branches/VuzeAndroidRemote_NoWebView/src/com/vuze/android/remote/rpc/TorrentAddedReceivedListener.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public interface TorrentAddedReceivedListener
 {
-	public void torrentAdded(Map mapTorrentAdded, boolean duplicate);
+	public void torrentAdded(Map<?, ?> mapTorrentAdded, boolean duplicate);
 
 	// TODO: pass original URL or name or something
 	public void torrentAddFailed(String message);
